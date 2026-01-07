b029c43f8958f56d6234aeaaed758c4fee0cdb6b
Mashtura Mazed <71812190+mashtura-m@users.noreply.github.com>
2026-01-06 23:45:24 -0800

    in-progress

---
+++ src/main/java/com/mashtura/tests/tests/DateManipulationTest.java
@@ -0,0 +1,11 @@
+package com.mashtura.tests.tests;
+
+import org.testng.annotations.Test;
+import com.mashtura.tests.base.BaseTest;
+
+public class DateManipulationTest extends BaseTest {
+    @Test
+    public void testDateManipulation() {
+        // Add date manipulation test logic here.
+    }
+}
