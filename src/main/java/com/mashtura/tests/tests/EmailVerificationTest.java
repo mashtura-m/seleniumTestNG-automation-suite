6c1922eff1d93c17f45ad9876fc8db4c12909cf5
Mashtura Mazed <71812190+mashtura-m@users.noreply.github.com>
2026-01-06 23:45:58 -0800

    in-progress

---
+++ src/main/java/com/mashtura/tests/tests/EmailVerificationTest.java
@@ -0,0 +1,11 @@
+package com.mashtura.tests.tests;
+
+import org.testng.annotations.Test;
+import com.mashtura.tests.base.BaseTest;
+
+public class EmailVerificationTest extends BaseTest {
+    @Test
+    public void testEmailVerification() {
+        // Add email verification test logic here.
+    }
+}
