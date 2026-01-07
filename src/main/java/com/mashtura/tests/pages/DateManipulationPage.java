ef55bf884f61dce18bc3dfc0ec40eebc6617790b
Mashtura Mazed <71812190+mashtura-m@users.noreply.github.com>
2026-01-06 23:39:32 -0800

    in-progress

---
+++ src/main/java/com/mashtura/tests/pages/DateManipulationPage.java
@@ -0,0 +1,9 @@
+package com.mashtura.tests.pages;
+
+/**
+ * Page class for date manipulation.
+ * Contains methods for interacting with date picker or date fields.
+ */
+public class DateManipulationPage {
+    // TODO: Implement date manipulation methods
+}
