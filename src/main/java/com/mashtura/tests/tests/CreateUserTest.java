9996742506316889b2c45869db98727e3905ad1a
Mashtura Mazed <71812190+mashtura-m@users.noreply.github.com>
2026-01-06 23:44:41 -0800

    in-progress

---
+++ src/main/java/com/mashtura/tests/tests/CreateUserTest.java
@@ -0,0 +1,11 @@
+package com.mashtura.tests.tests;
+
+import org.testng.annotations.Test;
+import com.mashtura.tests.base.BaseTest;
+
+public class CreateUserTest extends BaseTest {
+    @Test
+    public void testCreateUser() {
+        // Add user creation test logic here.
+    }
+}
