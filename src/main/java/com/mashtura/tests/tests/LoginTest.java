2962de376f7e4c11aff0d249e9228cd57c2243c8
Mashtura Mazed <71812190+mashtura-m@users.noreply.github.com>
2026-01-06 23:44:04 -0800

    in-progress

---
+++ src/main/java/com/mashtura/tests/tests/LoginTest.java
@@ -0,0 +1,10 @@
+package com.mashtura.tests.tests;
+import org.testng.annotations.Test;
+import com.mashtura.tests.base.BaseTest;
+
+public class LoginTest extends BaseTest {
+    @Test
+    public void testLogin() {
+        // Add login test logic here.
+    }
+}
